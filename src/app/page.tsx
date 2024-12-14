5ac7f1995d1529fb3a6eb3e513611e73d174347a
anhtr-kpt <anhtr.dev@gmail.com>
2024-12-14 22:10:38 +0700

    Build landing page #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { Check } from "lucide-react";
+import Phone from "@/components/Phone";
+import { Check, Star } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -42,8 +43,87 @@ export default function Home() {
                     <Check className="size-5 shrink-0 text-green-600" />
                     High-quality, durable material
                   </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="size-5 shrink-0 text-green-600" />5 year
+                    print guarantee
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="size-5 shrink-0 text-green-600" />
+                    Modern Iphone models supported
+                  </li>
                 </div>
               </ul>
+              <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
+                <div className="flex -space-x-4">
+                  <Image
+                    src="/users/user-1.png"
+                    alt="user image"
+                    width={100}
+                    height={100}
+                    className="inline-block size-10 rounded-full ring-2 ring-slate-100"
+                  />
+                  <Image
+                    src="/users/user-2.png"
+                    alt="user image"
+                    width={100}
+                    height={100}
+                    className="inline-block size-10 rounded-full ring-2 ring-slate-100"
+                  />
+                  <Image
+                    src="/users/user-3.png"
+                    alt="user image"
+                    width={100}
+                    height={100}
+                    className="inline-block size-10 rounded-full ring-2 ring-slate-100"
+                  />
+                  <Image
+                    src="/users/user-4.jpg"
+                    alt="user image"
+                    width={100}
+                    height={100}
+                    className="inline-block size-10 rounded-full ring-2 ring-slate-100"
+                  />
+                  <Image
+                    src="/users/user-5.jpg"
+                    alt="user image"
+                    width={100}
+                    height={100}
+                    className="inline-block object-cover size-10 rounded-full ring-2 ring-slate-100"
+                  />
+                </div>
+                <div className="flex flex-col justify-between items-center sm:items-start">
+                  <div className="flex gap-0.5">
+                    <Star className="size-4 text-green-600 fill-green-600" />
+                    <Star className="size-4 text-green-600 fill-green-600" />
+                    <Star className="size-4 text-green-600 fill-green-600" />
+                    <Star className="size-4 text-green-600 fill-green-600" />
+                    <Star className="size-4 text-green-600 fill-green-600" />
+                  </div>
+                  <p>
+                    <span className="font-semibold">1.250</span> happy customers
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+            <div className="relative md:max-w-xl">
+              <Image
+                src="/your-image.png"
+                width={100}
+                height={100}
+                alt="your image"
+                className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden 
+                sm:block lg:hidden xl:block"
+              />
+              <Image
+                src="/line.png"
+                alt="line image"
+                width={100}
+                height={100}
+                className="absolute w-20 -left-6 -bottom-6 select-none"
+              />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
             </div>
           </div>
         </MaxWidthWrapper>
